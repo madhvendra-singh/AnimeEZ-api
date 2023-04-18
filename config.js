00da77c417037c384a3f3b5037ad1279dcf2bbc9
Madhvendra Singh <86185962+madhvendra-singh@users.noreply.github.com>
2023-04-18 07:20:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  gogoanime_url: "https://ww2.gogoanime.com.co/", //gogoanime's website url
+  gogoanime_url: "https://ww2.gogoanime.com.co", //gogoanime's website url
   port: 6969, //Port for website.
 }
